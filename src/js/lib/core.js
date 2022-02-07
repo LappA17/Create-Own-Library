636f26c703698f7e1171216283e9686cbf2da897
Ruslan <ruslan.postoyuk@gmail.com>
2022-02-07 11:48:10 +0100

    41. Создаем анимации fadeIn/fadeOut

--- src/js/lib/core.js
+++ src/js/lib/core.js
@@ -1,30 +1,25 @@
-// Ядро нашей библиотеки
-
- const $ = function(selector) {
+const $ = function(selector) {
     return new $.prototype.init(selector);
 };
 
 $.prototype.init = function(selector) {
     if (!selector) {
-        return this; 
+        return this; // {}
     }
 
     if (selector.tagName) {
         this[0] = selector;
         this.length = 1;
-        return this; // фция прекратит свою работу и код дальше не пойдет там где object.assign и тд
+        return this;
     }
-/* Нам нужно узнать является ли селектор тегом, ведь если нам в this будет приходить элемент с хтмлколлекции то будет
-ошибка 
-this[0] = selector; - мы помещаем на 0 позицию тот элемент который приходит потому что у нас сам объект состоит
-их ключей и значений, здесь мы делаем тоже самое только вручную*/
 
-    Object.assign(this, document.querySelectorAll(selector));   
+    Object.assign(this, document.querySelectorAll(selector));
     this.length = document.querySelectorAll(selector).length;
-    return this; 
+    return this;
 };
-    $.prototype.init.prototype = $.prototype; 
 
-    window.$ = $;
+$.prototype.init.prototype = $.prototype;
 
-    export default $;
+window.$ = $;
+
+export default $;
--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,15 @@
-import './lib/lib';
+import $ from './lib/lib';
 
 $('button').on('click', function() {
-    $('div').eq(2).toggleClass('active');
+    $('div').eq(1).toggleClass('active');
 });
 
-$('div').click(function () { 
-    console.log($(this).index()); 
+$('div').click(function() {
+    console.log($(this).index());
 });
 
-// console.log($('div').eq(2).find('.more'));
+// console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findme'));
+$('button').fadeIn(1800); //  Теперь кнопка баттон появляется если fadeIn или исчезает если fadeOut через 1800 мс
 
-/* Мы главному диву, в котором все другие дивы добавили класс findme и допустим я хочу по селектору some найти родителя
-по селектору findme  */
-//console.log($('.some').closest('.findme')); 
-/* ./src/js/lib/core.js.$.init
-0: div.findme
-1: div.findme
-2: div.findme
-length: 3
-[[Prototype]]: Object
-Мы использовали фцию доллара на всех блоках some и у каждого из этих блоков мы попытались найти ближайший блок
-с классом findme и мы получили три раза одно и тоже значение потому что один родитель */
-
-console.log($('.more').eq(0).siblings()); // Этот метод не требует какого-то селектора
-/* ./src/js/lib/core.js.$.init {0: div.some, 1: div.some, 2: div.some, 3: div.more, length: 4}
-0: div.some
-1: div.some
-2: div.some
-3: div.more
-length: 4
-[[Prototype]]: Object 
-ПОЛУЧУ ВСЕХ СОСЕДЕЙ ЭТОГО 0 ДИВА
-А если пропишем не .more а findme то получим и кнопку, и див с классом актив, и скрипт, те все что внутри бади по родителю подходит*/
+// console.log($('button').html('Hello'));
