4d28f3c25b165655616b667f7240ae47b76640ad
Ruslan <ruslan.postoyuk@gmail.com>
2022-02-08 22:39:44 +0100

    44. Создаем Dropdown menu как готовый компонент

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,20 @@ $('[data-count="second"]').on('click', () => {
 $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 });
+
+/* Представь что нам нужно формулировать верстку динамически на основание того что нам приходит с бекенда 
+Скрипты которые были до этого не применяться вот к этой вот верстки. Те при клике ничего не произойдет на него.
+Но если мы подставим $('.dropdown-toggle').dropdown() после врапа то менюшка будет работать */
+$('.wrap').html(
+    `
+        <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
+            <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+                <a href="#" class="dropdown-item">Action</a>
+                <a href="#" class="dropdown-item">Action2</a>
+                <a href="#" class="dropdown-item">Action3</a>
+            </div>
+        </div> 
+    `
+);
+$('.dropdown-toggle').dropdown();
