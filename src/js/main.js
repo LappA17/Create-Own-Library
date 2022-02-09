5629992513d6dd7249e69bb0a21416ce65f9ef91
Ruslan <ruslan.postoyuk@gmail.com>
2022-02-09 19:37:25 +0100

    46. Динамическое создание модальных окон

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,46 @@
 import $ from './lib/lib';
 
-/* ТЕПЕРЬ ИЗ-ЗА МЕТОДА fadeToggle ПРИ НАЖАТИЕ ТЕКСТ ПРОПАДАЕТ КОГДА ОН БЫЛ, А КОГДА ЕГО НЕ БЫЛО ПОСЛЕ НАЖАТИЯ НА КНОПКУ - ОН ПОЯВЛЯЕТСЯ */
-// Пропадет первый текст
 $('#first').on('click', () => {
-    $('div').eq(1).fadeToggle(800); // eq(1), потому что это второй элемент, первый это див с контейнером
+    $('div').eq(1).fadeToggle(800);
 });
 
-// Пропадет второй текст
 $('[data-count="second"]').on('click', () => {
     $('div').eq(2).fadeToggle(800);
 });
 
-// Все два текста пропадут
 $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 });
 
-/* Представь что нам нужно формулировать верстку динамически на основание того что нам приходит с бекенда 
-Скрипты которые были до этого не применяться вот к этой вот верстки. Те при клике ничего не произойдет на него.
-Но если мы подставим $('.dropdown-toggle').dropdown() после врапа то менюшка будет работать */
-$('.wrap').html(
-    `
-        <div class="dropdown">
-            <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
-            <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
-                <a href="#" class="dropdown-item">Action</a>
-                <a href="#" class="dropdown-item">Action2</a>
-                <a href="#" class="dropdown-item">Action3</a>
-            </div>
-        </div> 
-    `
-);
-$('.dropdown-toggle').dropdown();
+$('#trigger').click(() => $('#trigger').createModal({ // метод createmodal будет вызываться только после клика на триггер
+    text: {
+        title: 'Modal title',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minus doloremque nesciunt enim rem quam corporis? Dolorem pariatur magnam distinctio perferendis. Ratione dolorem voluptates iusto facilis odit veritatis, suscipit voluptatibus!'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true // закрывает модельное окно
+            ],
+            [
+                'Save changes',
+                ['btn-success'],
+                false, // не закрывает мод окно
+                () => { // та самая коллбек фция
+                    alert('Данные сохранены');
+                }
+            ],
+            [
+                'Another btn',
+                ['btn-warning', 'ml-10'],
+                false,
+                () => {
+                    alert('Hello World');
+                }
+            ]
+        ]
+    }
+}));
